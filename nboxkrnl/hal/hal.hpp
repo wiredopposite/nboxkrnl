10411b4107bc3c9440921cd81f221a3616f6b43c
wiredopposite <jackxhorizon@gmail.com>
2026-05-21 09:21:26 -0600

    Add copyright info

--- nboxkrnl/hal/hal.hpp
+++ nboxkrnl/hal/hal.hpp
@@ -1,5 +1,6 @@
 /*
  * ergo720                Copyright (c) 2023
+ * wiredopposite		  Copyright (c) 2026
  */
 
 #pragma once
--- nboxkrnl/hal/halp.hpp
+++ nboxkrnl/hal/halp.hpp
@@ -1,5 +1,6 @@
 /*
  * ergo720                Copyright (c) 2022
+ * wiredopposite		  Copyright (c) 2026
  */
 
 #pragma once
